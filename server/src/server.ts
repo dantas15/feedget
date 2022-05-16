0f4adda506d9411fd7df32d15644e5d5b681c463
Gustavo Galote <gusgalot3g@gmail.com>
2022-05-16 02:19:18 -0300

    feat: create feedback

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,10 +1,27 @@
 import express from 'express';
 import { config } from 'dotenv';
+import { prisma } from './prisma';
 
 config();
 const PORT = process.env.PORT || 3333;
 
 const app = express();
+
+app.use(express.json());
+
+app.post('/feedbacks', async (request, response) => {
+  const { type, comment, screenshot } = request.body;
+
+  const feedback = await prisma.feedback.create({
+    data: {
+      type,
+      comment,
+      screenshot,
+    },
+  });
+
+  return response.status(201).json({ data: feedback });
+});
 
 app.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}/`);
