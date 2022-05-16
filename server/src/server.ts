40ae41c1c10af4abf2ae3ecf73d9da814dcf3cc5
Gustavo Galote <gusgalot3g@gmail.com>
2022-05-16 03:10:52 -0300

    feat: fake send mail

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import nodemailer from 'nodemailer';
 import { config } from 'dotenv';
 import { prisma } from './prisma';
 
@@ -9,6 +10,15 @@ const app = express();
 
 app.use(express.json());
 
+const transport = nodemailer.createTransport({
+  host: process.env.MAIL_HOST,
+  port: 2525,
+  auth: {
+    user: process.env.MAIL_AUTH_USER,
+    pass: process.env.MAIL_AUTH_PASS,
+  },
+});
+
 app.post('/feedbacks', async (request, response) => {
   const { type, comment, screenshot } = request.body;
 
@@ -18,6 +28,18 @@ app.post('/feedbacks', async (request, response) => {
       comment,
       screenshot,
     },
+  });
+
+  await transport.sendMail({
+    from: 'Feedget team <oi@feedget.com>',
+    to: 'Gustavo <gusgalot3g@gmail.com>',
+    subject: 'New Feedback',
+    html: [
+      `<div style="font-family: sans-serif; font-size=16px; color: '#333';">`,
+      `<h1>Feedback type: ${type}</h1>`,
+      `<p>Comment: ${comment}</p>`,
+      `</div>`,
+    ].join('\n'),
   });
 
   return response.status(201).json({ data: feedback });
